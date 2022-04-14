ff6109de61c3ab92de4443fcc98be9d8ac7f6797
= <=>
2022-04-14 07:52:15 +0200

    write error message

--- CheckForUpdates/CheckForUpdates.ps1
+++ CheckForUpdates/CheckForUpdates.ps1
@@ -246,10 +246,10 @@ try {
             }
             catch {
                 if ($directCommit) {
-                    throw "Failed to update AL-Go System Files. Make sure that the personal access token, defined in the secret called GhTokenWorkflow, is not expired and it has permission to update workflows."
+                    throw "Failed to update AL-Go System Files. Make sure that the personal access token, defined in the secret called GhTokenWorkflow, is not expired and it has permission to update workflows. (Error was $($_.Exception.Message))"
                 }
                 else {
-                    throw "Failed to create a pull-request to AL-Go System Files. Make sure that the personal access token, defined in the secret called GhTokenWorkflow, is not expired and it has permission to update workflows."
+                    throw "Failed to create a pull-request to AL-Go System Files. Make sure that the personal access token, defined in the secret called GhTokenWorkflow, is not expired and it has permission to update workflows. (Error was $($_.Exception.Message))"
                 }
             }
         }
