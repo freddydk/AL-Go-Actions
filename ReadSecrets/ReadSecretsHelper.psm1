da14c1b7a43e5ed6b3d4da80f9fcc14fe8aecc12
freddydk <freddyk@microsoft.com>
2022-05-27 22:50:25 +0200

    dump values

--- ReadSecrets/ReadSecretsHelper.psm1
+++ ReadSecrets/ReadSecretsHelper.psm1
@@ -23,6 +23,7 @@ function MaskValueInLog {
             throw "Secret $key contains characters, which are not supported in AL-Go for GitHub. This exception is thrown to avoid that the secret is revealed in the log."
         }
         else {
+            Write-Host $chint
             $val2 += $script:escchars[$chint-32]
         }
     }
